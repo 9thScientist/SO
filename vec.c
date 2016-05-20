00f6e8d5ec4283d89569917e5c9cd5a50503c37b
9thScientist <bc.tudo@gmail.com>
2016-05-20 22:05:39 +0000

    Added github link to vec

--- vec.c
+++ vec.c
@@ -3,6 +3,8 @@
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the MIT license. See LICENSE for details.
+ *
+ * github: https://github.com/rxi/vec
  */
 
 #include "vec.h"
